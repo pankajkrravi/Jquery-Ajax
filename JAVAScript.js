f612c1842207a35c0725bafcaa48857afb61d9f5
Pankaj <pankaj.ravi@goomo.com>
2019-12-01 11:51:34 +0530

    Ajax_jquery_First _commit

---
+++ JAVAScript.js
@@ -0,0 +1,78 @@
+<html>
+    <header>
+        
+    </header>
+    <body>
+<h1>Learn JSON ON WEB DEMO !!</h1>
+  <div id='output'></div>
+  <script type="text/javascript">
+    var data='{
+  results: [
+    {
+      gender: "female",
+      name: {
+        title: "Ms",
+        first: "Pinja",
+        last: "Tervo"
+      },
+      location: {
+        street: {
+          number: 4804,
+          name: "Pispalan Valtatie"
+        },
+        city: "Äänekoski",
+        state: "Central Ostrobothnia",
+        country: "Finland",
+        postcode: 33074,
+        coordinates: {
+          latitude: "-5.7311",
+          longitude: "89.7099"
+        },
+        timezone: {
+          offset: "-10:00",
+          description: "Hawaii"
+        }
+      },
+      email: "pinja.tervo@example.com",
+      login: {
+        uuid: "cea179f1-4839-4438-aa31-65297a6fbbe6",
+        username: "bigkoala389",
+        password: "caroline",
+        salt: "GlKslF5O",
+        md5: "657986481359d9d73441792628bfef0e",
+        sha1: "1fcbfc47ff7c73cfc4cdd1e834a88063639417a4",
+        sha256: "73e7bd9e7febebe9fd3d0598039819d1e237ab341e32dc7268e8056cf8c2024c"
+      },
+      dob: {
+        date: "1951-07-27T07:03:59.794Z",
+        age: 68
+      },
+      registered: {
+        date: "2013-01-28T17:32:55.291Z",
+        age: 6
+      },
+      phone: "07-497-694",
+      cell: "049-147-16-52",
+      id: {
+        name: "HETU",
+        value: "NaNNA222undefined"
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/45.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/45.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/45.jpg"
+      },
+      nat: "FI"
+    }
+  ],
+  info: {
+    seed: "93d085d08a1d2415",
+    results: 1,
+    page: 1,
+    version: "1.3"
+  }
+}';
+console.log(data)
+</script>
+    </body>
+</html>
---
+++ JsonOnWeb.js
@@ -0,0 +1,42 @@
+<html>
+    <header>
+        
+    </header>
+    <body>
+<h1>Learn JSON ON WEB DEMO !!</h1>
+  <div id='output'></div>
+  <script type="text/javascript">
+     
+     var daata='{
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}';
+console.log(daata)
+
+
+
+
+
+    // var x='sss';
+    // console.log(x)
+</script>
+    </body>
+</html>
